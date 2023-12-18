7aa29bff5a4ed885146e955196c96e1f292b0b15
Jakob Krebs <jakob@krbs.me>
2023-12-18 13:05:07 +0000

    update run script

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+if [ "$1" == "deploy" ]; then
+       rsync -avr . tofino1:jakob/cap-mgr-cp/
+       exit
+fi
+
 make
 
 sudo bash <<EOF
-export SDE_INSTALL=/home/jakob/bf-sde-9.13.0/install
+export SDE=/home/ubuntu/bf-sde-9.11.0
+export SDE_INSTALL=$SDE/install
 export LD_LIBRARY_PATH=/usr/local/lib:$SDE_INSTALL/lib:$LD_LIBRARY_PATH
 export GLOG_minloglevel=0
 export GLOG_logtostderr=1
-./control-plane --config=cap-manager --interface=veth9 --address=10.0.0.9 --port=1234
+./control-plane --config=cap-manager --interface=enx020000000002 --address=10.0.10.2 --port=1234
 EOF
